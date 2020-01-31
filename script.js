b558b00c0844a766d2c2b7d237571544a34d9c33
Brett Sevy <justbrettt@gmail.com>
2020-01-31 07:57:52 -0600

    time and day working and start of hour blocks added

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-// time and date
+/*jshint esversion: 6 */
 
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+    // append day in moment.js in #currentDay header
+
+   { var day = moment().format("llll");
+    $("#currentDay").append(day);
     
-// color change with time
-    // grey past hours
-    // red current hour
-    // green upcoming hours
+}
 
-// save buttons at the end of time blocks
-    //locale storage what is typed in time blocks when save button clicked
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// need a container for each hour needed
 
-    // {var num1 = ("help");
+    // var num1 = 10;
     // var num2 = 20;
-    // var num3 = 30;}
+    // var num3 = 30;
 
-    // $(".container").append(`
+    // $(`
     // <section><span>${num1}</span></section>
     // <section><span>${num2}</span></section>
-    // <section><span>${num3}</span></section>`);
+    // <section><span>${num3}</span></section>
+    // `).appendTo(".container");
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+    // for loop the time blocks so they color out as the time runs
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+    // input text area in the time block onclick
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
+
+
 
