d7a36e84c098d1655701ac93491e6c331a42699f
Brett Sevy <justbrettt@gmail.com>
2020-01-30 19:01:17 -0600

    adding files and starting point

---
+++ script.js
@@ -0,0 +1,20 @@
+// time and date
+
+    
+// color change with time
+    // grey past hours
+    // red current hour
+    // green upcoming hours
+
+// save buttons at the end of time blocks
+    //locale storage what is typed in time blocks when save button clicked
+
+    // {var num1 = ("help");
+    // var num2 = 20;
+    // var num3 = 30;}
+
+    // $(".container").append(`
+    // <section><span>${num1}</span></section>
+    // <section><span>${num2}</span></section>
+    // <section><span>${num3}</span></section>`);
+
