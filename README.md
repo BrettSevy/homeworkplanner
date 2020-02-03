d4de023c99926e2267ab3fc89af34b8948dd5675
Brett Sevy <justbrettt@gmail.com>
2020-02-02 20:43:03 -0600

    updated with rolling time and text blocks that change color, read me also updated

--- README.md
+++ README.md
@@ -1,36 +1,15 @@
 # homeworkplanner
 
-# 05 Third-Party APIs: Work Day Scheduler
+Work Day Scheduler
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+A simple calendar application that allows the user to save events for each hour of the day. As a indivual with a busy schedule you can add important events to a daily planner so that you can manage your time more effectively
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-## User Story
-
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
-
-```
-<!-- GIVEN I am using a daily planner to create a schedule -->
-<!-- WHEN I open the planner -->
-<!-- THEN the current day is displayed at the top of the calendar -->
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+the current day is displayed at the top of the calendar
+You am presented with timeblocks for standard business hours
+You view the timeblocks for that day
+each timeblock is color coded to indicate whether it is in the past, present, or future
+You can enter an event into a timeblock
+You click the save button for that the saved events persist
 
 
 
--- script.js
+++ script.js
@@ -1,42 +1,57 @@
 /*jshint esversion: 6 */
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-    // append day in moment.js in #currentDay header
 
+    
+// header time
    { var day = moment().format("llll");
     $("#currentDay").append(day);
     
 }
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// need a container for each hour needed
+//view the timeblocks for that day
+var momentTime = moment().startOf("day").add(7, "hour");
+var hour = moment().format("H");
 
-    // var num1 = 10;
-    // var num2 = 20;
-    // var num3 = 30;
+for(var i = 8; i<20; i++)
 
-    // $(`
-    // <section><span>${num1}</span></section>
-    // <section><span>${num2}</span></section>
-    // <section><span>${num3}</span></section>
-    // `).appendTo(".container");
+{
+    var timeSlot = momentTime.add(1, "hour").format("HH:mm A");
+    var currentState;
+    
+    // timeblock is color coded to indicate whether it is in the past, present, or future
+        if(hour == i) {
+            currentState = 'present';
+        } else if (hour > i) {
+            currentState = 'future';
+        } else if (hour < i) {
+            currentState = 'past';
+        }
+        
+        var newStuff =
+        `<container class="row" >
+            <div class="col-2"></div>
+            <div class="hour w-25 p-4 col-1" id=time>${timeSlot}</div>
+            <textarea class="w-50 p-4 col-6 ${currentState}"></textarea>
+            <button class="saveBtn w-25 p-4 col-1 far fa-save"></button>
+            <div class="col-2">
+            </div>
+        </container>`
+        ;
+
+        $("#containAll").append(newStuff);
+    }
+    
+    // WHEN I click the save button for that timeblock
+    // THEN the text for that event is saved in local storage
+    // WHEN I refresh the page
+    // THEN the saved events persist
+    
 
 
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-    // for loop the time blocks so they color out as the time runs
+    
+   
+    
 
-// WHEN I click into a timeblock
-// THEN I can enter an event
-    // input text area in the time block onclick
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
 
 
 
