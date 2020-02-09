b6d695f3e979f5cc43057d122b027884808ce765
Brett Sevy <justbrettt@gmail.com>
2020-02-08 22:10:29 -0600

    added line between blocks and bigger save icon

--- script.js
+++ script.js
@@ -31,10 +31,11 @@ for (var i = 8; i < 20; i++) {
             <div class="col-2"></div>
             <div class="hour w-25 p-4 col-1">${timeSlot}</div>
             <textarea class="description w-50 p-4 col-6 ${currentState} hour-${i}"></textarea>
-            <button class="saveBtn w-25 p-4 col-1 far fa-save"></button>
+            <button class="saveBtn w-25 p-4 col-1 fas fa-save fa-2x"></button>
             <div class="col-2">
-            </div>
-        </container>`
+            </div>  
+        </container>
+        <p></p>`
         ;
 
     $("#containAll").append(newStuff);
